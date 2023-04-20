ded3cc89e278da42d06980cccf57a42244463470
fansuregrin <pwz113436@gmail.com>
2023-04-20 17:09:24 +0800

    add minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    // If the `Result` is an `Ok` value, this method’s behavior is similar to `unwrap`:
+    // it returns the inner value `Ok` is wrapping. However, if the value is an `Err` value,
+    // this method calls the code in the closure, which is an anonymous function we define
+    // and pass as an argument to `unwrap_or_else`.
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        // The `process::exit` function will stop the program immediately
+        // and return the number that was passed as the exit status code.
+        process::exit(1);
+    });
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
+}
