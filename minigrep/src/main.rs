d0c11e1973f7d368de98d9883f41001194bc3280
fansuregrin <pwz113436@gmail.com>
2023-04-20 19:40:23 +0800

    update minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,17 +11,14 @@ fn main() {
     // this method calls the code in the closure, which is an anonymous function we define
     // and pass as an argument to `unwrap_or_else`.
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         // The `process::exit` function will stop the program immediately
         // and return the number that was passed as the exit status code.
         process::exit(1);
     });
-    
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
